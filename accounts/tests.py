125641b472fe2d404d7d48d3eb3edd2dc270e08a
Firstname Lastname <you@example.com>
2024-07-18 13:21:59 -0500

    added signup page, and implemented login and logout func

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 # Create your tests here.
+
 
 class UsersManagersTests(TestCase):
     def test_create_user(self):
@@ -15,7 +18,7 @@ class UsersManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-    
+
     def test_create_superuser(self):
         User = get_user_model()
         user = User.objects.create_superuser(
@@ -28,4 +31,30 @@ class UsersManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
-    
+
+
+class SignupPageTests(TestCase):
+    def test_signup_url_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "test@email.com",
+                "password1": "passwoord123",
+                "password2": "passwoord123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "test@email.com")
+
